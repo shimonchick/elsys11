b2ded85f3c58a1132142de938ea717cad25cdd6a
shimonchick <shimonchick@gmail.com>
2018-10-17 08:13:54 +0300

    OOP

--- OOP/school/2018-10-11/ex1.cpp
+++ OOP/school/2018-10-11/ex1.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main(){
     string word;
-    int sum_global_chars = 0, char_count = 0, words_count = 0, sentences_count = 0  
+    int sum_global_chars = 0, char_count = 0, words_count = 0, sentences_count = 0 ; 
     while(word != "END"){
+    	cout<< "reading" << endl;
         if(cin.eof()) break;
         cin >> word;
         
---
+++ OOP/school/2018-10-16/const.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class Point{
+    private:
+        double x_;
+        double y_;
+
+    public:
+        Point(double x, double y){
+            x_ = x;
+            y_ = y;
+        }
+        int get_x() const{
+            return x_;
+        }
+        int get_y() const{
+            return y_;
+        }
+        Point& set_x(double x){
+            x_ = x;
+            return *this;
+        }
+        Point& set_y(double y){
+            y_ = y;
+            return *this;
+        }
+
+};
+
+int main(){
+
+    const Point origin(0, 0);
+    //origin.set_x(2);
+    origin.get_x();
+
+    Point point(3, 5);
+
+    point.set_x(11).set_y(11);
+
+    Point& point2 = point.set_x(4);
+    point2.set_x(6);
+    cout<< point2.get_x() << endl;
+    cout << point.get_x() << " , " << point.get_y();
+    return 0;
+}
