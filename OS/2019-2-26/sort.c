e5294a1c4efe7f0b4377022ce3cff876c489d970
shimonchick <shimonchick@gmail.com>
2019-03-12 22:17:07 +0200

    OS multithreaded sort

--- OS/2019-2-26/sort.c
+++ OS/2019-2-26/sort.c
@@ -3,12 +3,25 @@
 #include <pthread.h>
 
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
-
+//------------------------------------------------------------------------
+// NAME: Mihail Kirov
+// CLASS: XIb
+// NUMBER: 19
+// PROBLEM: #1
+// FILE NAME: sort.c
+// FILE PURPOSE:
+// sorts an array on multiple threads
+//------------------------------------------------------------------------
 typedef struct{
     int* data;
     int size;
 } list;
-
+//------------------------------------------------------------------------
+// FUNCTION: sort_array
+// sorts a given array using bubble sort
+// PARAMETERS:
+// void* arg --> pointer to the data to be sorted
+//------------------------------------------------------------------------
 void* sort_array(void* arg){
     list* list1 = (list*)arg;
     for(int i = 0; i < list1->size - 1; i++){
@@ -23,24 +36,39 @@ void* sort_array(void* arg){
     
     return NULL;
 }
-// argv[1] = number of array elements
-// argv[2] = number of threads
-int main(int argc, char* argv[]){
-    //generate random array
+
+//generates an array of @count elements in dynamic memory
+//array must be freed manually
+int* generate_random_array(int count){
+
     time_t t;
     srand((unsigned) time(&t));
-    int count = atoi(argv[1]);
-    printf("count: %d\n", count);
     int* data = malloc(sizeof(int) * count);
     
     for(int i = 0; i < count; i++){
         data[i] = rand() % 100;    
         printf("%d ", data[i]);
     }
-    printf("\ntest 1\n");
-    int thread_count = MIN(atoi(argv[2]), count);
+    return data;
+}
+
+//------------------------------------------------------------------------
+// FUNCTION: main
+// generates, partitions and merges the array to be sorted
+// PARAMETERS:
+// argv[1] = number of array elements
+// argv[2] = number of threads
+//------------------------------------------------------------------------
+int main(int argc, char* argv[]){
+    //generate random array
+    int count = atoi(argv[1]);
+    int* data = generate_random_array(count);
+
+    int thread_count = MIN(atoi(argv[2]), atoi(argv[1])); // min of number of threads and number of elements;
+    
     pthread_t threads[thread_count];
     
+    //partition the data to be sorted and call sort function on separate thread
     list partitions[thread_count];
     for(int i = 0; i < thread_count; i++){
         partitions[i].data = data + (count/thread_count)*i;
@@ -48,11 +76,11 @@ int main(int argc, char* argv[]){
         pthread_create(&threads[i], NULL, sort_array, &partitions[i]);
     }
 
-    
+    //wait for all threads to finish
     for(int i = 0; i < thread_count; i++){
         pthread_join(threads[i], NULL);
     }
-    
+    //test prints
     for(int i = 0; i < thread_count; i++){
         printf("\npartition %d values:\n", i);
         for(int j = 0; j < partitions[i].size; j++){
@@ -60,16 +88,28 @@ int main(int argc, char* argv[]){
         }
     } 
     printf("\n");
+
     int indexes[thread_count];
     //default index values;
     for(int i = 0; i < thread_count; i++){
         indexes[i] = 0;
     }
+    //merge all partitions
     int result[count];
     for(int i = 0; i < count; i++){
-        int min_index = 0;
+        int min_index;
+        for(int j = 0; j < thread_count; j++){
+            if(indexes[j] < count/thread_count){
+                min_index = j;
+                break;
+            }
+        }
         for(int j = 1; j < thread_count; j++){
             if(indexes[j] == count/thread_count){
+                continue;
+
+            }
+            if(indexes[min_index] == count/thread_count){
                 continue;
             }
             if(partitions[j].data[indexes[j]] < partitions[min_index].data[indexes[min_index]]){
@@ -82,18 +122,7 @@ int main(int argc, char* argv[]){
         }
         
     }
- //   int index_first = 0, index_second = 0;
- //   int result[count];
- //   for(int i = 0; i < count; i++){
- //       if(list1.data[index_first] < list2.data[index_second]){
- //           result[i] = list1.data[index_first];
- //           index_first++;
- //       }
- //       else{
- //           result[i] = list2.data[index_second];
- //           index_second++;
- //       }
- //   }
+    //print the result
     for(int i = 0; i < count; i++){
         printf("%d ", result[i]);
     }
